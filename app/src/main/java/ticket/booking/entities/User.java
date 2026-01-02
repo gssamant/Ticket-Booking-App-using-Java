0aba61acb6d89b4522fcd7b373ee065edac2df14
GauravSinghSamant <thorusamant8@gmail.com>
2026-01-02 11:54:33 +0530

    Initial Commit

---
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -0,0 +1,10 @@
+package ticket.booking.entities;
+
+public class User {
+    private String name;
+    private String password;
+    private String hashedPassword;
+    private List<Ticket> bookedTickets;
+    private String userId;
+
+}
