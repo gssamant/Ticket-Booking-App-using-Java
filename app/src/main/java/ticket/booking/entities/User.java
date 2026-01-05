dd730aa1ffb9d59a103ebce8ff73b1009341a988
GauravSinghSamant <thorusamant8@gmail.com>
2026-01-05 11:31:16 +0530

    Implemented methods

--- app/src/main/java/ticket/booking/entities/User.java
+++ app/src/main/java/ticket/booking/entities/User.java
@@ -1,10 +1,64 @@
 package ticket.booking.entities;
 
+import java.util.List;
+
 public class User {
+
     private String name;
     private String password;
     private String hashedPassword;
     private List<Ticket> bookedTickets;
     private String userId;
+
+    public User() {}
+
+    public User(String name, String password, String hashedPassword, List<Ticket> bookedTickets, String userId) {
+        this.name = name;
+        this.password = password;
+        this.hashedPassword = hashedPassword;
+        this.bookedTickets = bookedTickets;
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+    }
+
+    public List<Ticket> getBookedTickets() {
+        return bookedTickets;
+    }
+
+    public void setBookedTickets(List<Ticket> bookedTickets) {
+        this.bookedTickets = bookedTickets;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
 
 }
